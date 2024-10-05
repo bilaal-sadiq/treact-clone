93e008b8dabdf417a3a82147e2416a60d73689b3
mrmbs1 <mbilalsadiq.19@gmail.com>
2024-10-05 23:22:28 +0100

    faq

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ function openMenu() {
 function closeMenu() {
     document.body.classList.remove('menu--open')
 }
+
+
+const openFAQ1 = () => {
+    document.getElementById('faq1').classList.toggle("faq__expand");
+    document.getElementById('arrow1').classList.toggle("faq__arrow--up");
+}
+
+const openFAQ2 = () => {
+    document.getElementById('faq2').classList.toggle("faq__expand");
+    document.getElementById('arrow2').classList.toggle("faq__arrow--up");
+}
+
+const openFAQ3 = () => {
+    document.getElementById('faq3').classList.toggle("faq__expand");
+    document.getElementById('arrow3').classList.toggle("faq__arrow--up");
+}
